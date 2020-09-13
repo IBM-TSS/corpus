2f5481ac93d52cb2e07574238d90b9398620e614
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2020-09-12 19:06:05 -0500

    quick templ api

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -19,6 +21,7 @@ type Ticket struct {
 }
 
 var client *mongo.Client
+var long_run_client *mongo.Client
 var err error
 
 func CreateTicketEndpoint(response http.ResponseWriter, request *http.Request) {
@@ -36,6 +39,61 @@ func CreateTicketEndpoint(response http.ResponseWriter, request *http.Request) {
 	json.NewEncoder(response).Encode(result)
 }
 
+func GetUnAssignedTicket(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-Type", "application/json")
+	collection := client.Database("tickets").Collection("not_assigned")
+	bdi_collection := long_run_client.Database("santander").Collection("bdi")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancel()
+	
+	var result []bson.M
+	var err error
+	var cursor *mongo.Cursor
+	
+	cursor, err = collection.Find(ctx, bson.D{})
+	defer cursor.Close(ctx)
+
+	if err != nil {
+		// ErrNoDocuments means that the filter did not match any documents in the collection
+		if err == mongo.ErrNoDocuments {
+			log.Fatal("noo doocss")
+			return
+		}
+		log.Fatal("fui")
+		log.Fatal(err)
+	}
+
+	for cursor.Next(ctx) {
+		var temp bson.M
+		var atm bson.M
+
+		cursor.Decode(&temp)
+		// Find the ATM
+		err = bdi_collection.FindOne(ctx, bson.D{{"_id", temp["atm"]}}).Decode(&atm)
+		if err != nil {
+			// ErrNoDocuments means that the filter did not match any documents in the collection
+			if err == mongo.ErrNoDocuments {
+				log.Fatal("noo doocss")
+				return
+			}
+			log.Fatal(temp)
+			log.Fatal(err)
+		}
+
+		temp["atm"] = atm
+		result = append(result, temp)
+
+	}
+
+	json.NewEncoder(response).Encode(result)
+}
+
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
+
+
 func main() {
 	fmt.Println("Starting")
 
@@ -49,10 +107,21 @@ func main() {
 		log.Fatal(err)
 	}
 
+	long_run_client, err = mongo.Connect(ctx, options.Client().ApplyURI(
+		"mongodb+srv://eduardoSantos:05120714@cluster0.mx9ah.mongodb.net/santander?retryWrites=true&w=majority",
+	))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	router := mux.NewRouter()
 
+	
 	router.HandleFunc("/ticket", CreateTicketEndpoint).Methods("POST")
+	// router.HandleFunc("/ticket/{_id}", GetUnAssignedTicket).Methods("GET")
+	router.HandleFunc("/ticket", GetUnAssignedTicket).Methods("GET")
 
-	http.ListenAndServe(":9707", router)
-
+	handler := cors.AllowAll().Handler(router)
+	log.Fatal(http.ListenAndServe(":9707", handler))
 }
